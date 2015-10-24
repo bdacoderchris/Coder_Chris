8fd20fcb44c6bdc94cb58bf8881785fcd44c6534
Christopher Ragon <christopher.ragon@gmail.com>
2015-10-24 17:55:28 -0500

    Initial Commit
    New project and packages installed

---
+++ TileEstimator/TileEstimator/AppJs/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('app', ['ngRoute']);
+
+app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider){
+    $routeProvider
+    .when('/home',{
+        templateUrl: 'AppJs/Views/home.html'
+    })
+    .otherwise({
+        redirectTo: '/home'
+    });
+}]);
