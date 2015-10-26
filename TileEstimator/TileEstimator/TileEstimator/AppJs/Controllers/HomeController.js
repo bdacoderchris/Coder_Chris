432350da3d46e713b8d466d5290f85200fb4f3e4
Christopher Ragon <christopher.ragon@gmail.com>
2015-10-25 23:34:25 -0500

    Updated Calculator and added CSS

---
+++ TileEstimator/TileEstimator/TileEstimator/AppJs/Controllers/HomeController.js
@@ -0,0 +1,44 @@
+﻿app.controller('HomeController', ['$scope', function ($scope) {
+
+    $scope.dimensions = {
+        widthFeet: "",
+        widthInches: 0,
+        depthFeet: "",
+        depthInches: 0
+    }
+
+    $scope.groutThickness = {};
+    $scope.grout = [
+        { id: 1, name: "1/4 inch", value: .063 },
+        { id: 2, name: "1/8 inch", value: .016 },
+        { id: 3, name: "1/16 inch", value: .0040 }
+    ];
+    $scope.groutThickness.selected = {};
+
+    $scope.tileSize = {};
+    $scope.tile = [
+       { id: 1, name: '12x12', value: 144 },
+       { id: 2, name: '18x18', value: 324 },
+       { id: 3, name: '24x24', value: 576 },
+       { id: 4, name: '10x16', value: 160 },
+       { id: 5, name: '10x20', value: 200 },
+       { id: 6, name: '12x24', value: 288 },
+    ];
+    $scope.tileSize.selected = {};
+
+    $scope.roomArea = function () {
+        var width = ($scope.dimensions.widthFeet * 12) + ($scope.dimensions.widthInches);
+        var depth = (($scope.dimensions.depthFeet * 12) + $scope.dimensions.depthInches);
+        $scope.result = ((width * depth) / (144 - $scope.groutThickness.selected.value)) * 1.10;
+        $scope.numTiles = ((width * depth) / ($scope.tileSize.selected.value - $scope.groutThickness.selected.value)) * 1.10;
+
+        $scope.dimensions = {
+            widthFeet: "",
+            widthInches: 0,
+            depthFeet: "",
+            depthInches: 0
+        }
+        $scope.tileSize = {};
+        $scope.groutThickness = {};
+    }
+}])
---
+++ TileEstimator/TileEstimator/TileEstimator/AppJs/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('app', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when('/home',{
+        templateUrl: 'AppJs/Views/home.html'
+    })
+    .otherwise({
+        redirectTo: '/home'
+    });
+}]);
+
