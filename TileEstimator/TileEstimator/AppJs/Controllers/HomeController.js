5d564aa5dedee6322919be2222fdf225c44b05fe
Christopher Ragon <christopher.ragon@gmail.com>
2015-10-25 20:07:48 -0500

    Calculation working
    Added drop down for tile size and default waste percentage of 10%

--- TileEstimator/TileEstimator/AppJs/Controllers/HomeController.js
+++ TileEstimator/TileEstimator/AppJs/Controllers/HomeController.js
@@ -1,14 +1,22 @@
 ﻿app.controller('HomeController', ['$scope', function ($scope) {
-    $scope.widthFeet;
-    $scope.depthFeet;
-    $scope.widthInches;
-    $scope.depthInches;
-    
+    $scope.dimensions = {
+        widthFeet: "",
+        widthInches: "",
+        depthFeet: "",
+        depthInches: ""
+    }
+    $scope.tileSize = {};
+    $scope.tile = [
+       { id: 1, name: '12x12', value: 144 },
+       { id: 2, name: '18x18', value: 324 },
+       { id: 3, name: '24x24', value: 576 }
+    ];
+    $scope.tileSize.selected = {};
 
     $scope.roomArea = function () {
-        var width = ($scope.widthFeet * 12) + $scope.widthInches;
-        var depth = ($scope.depthFeet * 12) + $scope.depthInches;
-        $scope.result = width * depth;
-        console.log($scope.result);
+        var width = ($scope.dimensions.widthFeet * 12) + ($scope.dimensions.widthInches);
+        var depth = (($scope.dimensions.depthFeet * 12) + $scope.dimensions.depthInches);
+        $scope.result = ((width * depth) / $scope.tileSize.selected.value) * 1.10;
+        console.log($scope.tileSize.selected.value);
     }
 }])
